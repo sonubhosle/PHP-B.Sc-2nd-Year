b9d11b389385693ae20835a8915761391a8deb47
sonubhosle <sbhosle1011@gmail.com>
2026-01-19 10:10:03 +0530

    php programs

---
+++ array.php
@@ -0,0 +1,9 @@
+<?php  
+
+    $fruits = array("Apple","Mango","Berry","Banana");
+
+    echo $fruits[0] ."<br>";
+    echo $fruits[1] ."<br>";
+    echo $fruits[2] ."<br>";
+    echo $fruits[3] ."<br>";
+?>
---
+++ associatearray.php
@@ -0,0 +1,18 @@
+<h1>write a code to demonstrate accociated array in php <h1>
+
+
+<?php  
+     $student = array(
+        "name"=> "Ajay",
+        "class" => "B.Sc Cs",
+        "age" => 20,
+        "rollNo" => 12,
+        "city" => "nanded",
+     );
+
+     echo "name :" . $student['name'] . "<br>";
+     echo "class :" . $student['class'] . "<br>";
+     echo "age :" . $student['age'] . "<br>";
+     echo "rollNo :" . $student['rollNo'] . "<br>";
+     echo "city :" . $student['city'] . "<br>";
+?> 
