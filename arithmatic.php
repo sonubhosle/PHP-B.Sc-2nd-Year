05015a959112a93e67c1d68aba9eacb47f5855e0
sonubhosle <sbhosle1011@gmail.com>
2026-01-21 09:52:50 +0530

    Arithmatic OP

---
+++ arithmatic.php
@@ -0,0 +1,13 @@
+<?php
+
+ echo "Arithmatic Operators";
+ echo "<br>";
+   $a = 29;
+   $b = 34;
+
+    echo "Add = ". ($a + $b) . "<br>";
+    echo "Sub = ". ($a - $b) . "<br>";
+    echo "Div = ". ($a / $b) . "<br>";
+    echo "Mul = ". ($a * $b) . "<br>";
+    echo "Rem = ". ($a % $b) . "<br>";
+?>
--- index.php
+++ index.php
@@ -2,5 +2,5 @@
 <?php require('header.php') ?>
 
 <?php  echo "Welcome" ?> 
-
+ <?php include "arithmatic.php"?>
 <?php include('footer.php') ?>
